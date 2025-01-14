ac5c3adade003a808af14ba9687451d286ff1424
bur-lis <byrakova.lis@gmail.com>
2025-01-14 15:29:18 +0300

    add humster page

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
+import {HumsterPage} from './pages/Humster'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <HumsterPage/>
   </React.StrictMode>
 )
