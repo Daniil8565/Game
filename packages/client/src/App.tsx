da34158c083b6690a5268d42e40f1a820a43b8eb
Daniil Pereskokov <129772326+Daniil8565@users.noreply.github.com>
2025-01-15 17:12:59 +0300

    фикс тестов

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,13 +1,13 @@
-import { useEffect } from 'react'
-import UserData from './Pages/UserData'
+// import { useEffect } from 'react'
+// import UserData from './Pages/UserData'
 import { SigninPage } from './Pages/AuthPages/SigninPage'
-import { SignupPage } from './Pages/AuthPages/SignupPage'
+// import { SignupPage } from './Pages/AuthPages/SignupPage'
 import './App.css'
 
 function App() {
   return (
     <>
-      <SignupPage />
+      <SigninPage />
     </>
   )
 }
