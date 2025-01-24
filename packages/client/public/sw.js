7fa1beebbbef4b8fd1f8352afb48812bec943afc
Daniil Pereskokov <129772326+Daniil8565@users.noreply.github.com>
2025-01-24 13:47:35 +0300

    выполнена задача с sw

--- packages/client/public/sw.js
+++ packages/client/public/sw.js
@@ -1,33 +1,10 @@
-const CACHE_NAME = 'my-site-cache-v5'
+const CACHE_NAME = 'my-site-cache-v1'
 
 const URLS = [
-  '/src/main.tsx',
-  '/src/App.tsx',
-  '/src',
-  '/src/App.css',
-  '/src/components/AuthButton/AuthButton.module.scss',
-  '/src/components/AuthButton/AuthButton.tsx',
-  '/src/components/AuthButton/index.ts',
-  '/src/components/AuthInput/AuthInput.module.scss',
-  '/src/components/AuthInput/AuthInput.tsx',
-  '/src/components/AuthInput/index.ts',
-  '/src/components/ProfileErrorMessage/ErrorMessage.module.scss',
-  '/src/components/ProfileErrorMessage/ErrorMessage.tsx',
-  '/src/index.css',
-  '/src/pages/AuthPages/SigninPage/SigninPage.tsx',
-  '/src/pages/AuthPages/SigninPage/SinginPage.module.scss',
-  '/src/pages/AuthPages/SigninPage/index.ts',
-  '/src/pages/AuthPages/components/AuthForm/AuthForm.module.scss',
-  '/src/pages/AuthPages/components/AuthForm/AuthForm.tsx',
-  '/src/pages/AuthPages/components/AuthForm/index.ts',
-  '/src/pages/StartPage/StartPage.module.scss',
-  '/src/pages/StartPage/StartPage.tsx',
-  '/src/pages/StartPage/index.ts',
-  '/src/serviceWorker.ts',
+  '/index.html',
   '/vite.svg',
-  '/client',
-  '/@react-refresh',
-  '/@vite/client',
+  '/assets/index.386e715f.css',
+  '/assets/index.2743df7b.js',
 ]
 
 this.addEventListener('install', event => {
@@ -65,14 +42,12 @@ this.addEventListener('activate', function (event) {
         )
       })
       .then(() => {
-        console.log('New cache is ready!')
         return self.clients.claim()
       })
   )
 })
 
 this.addEventListener('fetch', event => {
-  console.log('Сделан запрос')
   event.respondWith(
     caches.match(event.request).then(response => {
       if (response) {
