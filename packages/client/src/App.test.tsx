a4887e3edc2eecb249098ebb1e032409285c3663
Волков Никита Игоревич <nivolkov@MacBook-Pro-Volkov.local>
2025-01-24 18:25:02 +0300

    RI-7: Убрать тест

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -7,8 +7,3 @@ const appContent = 'Авторизация'
 global.fetch = jest.fn(() =>
   Promise.resolve({ json: () => Promise.resolve('hey') })
 )
-
-test('Example test', async () => {
-  render(<App />)
-  expect(screen.getByText(appContent)).toBeDefined()
-})
