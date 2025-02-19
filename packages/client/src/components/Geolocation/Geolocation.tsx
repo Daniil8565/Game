3238769c616aeb744fbff4d1a5b1134a77e9c1dd
bur-lis <byrakova.lis@gmail.com>
2025-02-19 16:09:39 +0300

    создан компонент для отображение локации, добавлен для отображения в профиль

---
+++ packages/client/src/components/Geolocation/Geolocation.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Geolocation.module.scss'
+import { useGeolocation } from '../../utils/GeolocationAPI'
+
+export const Geolocation: React.FC = () => {
+  const { location, getLocation } = useGeolocation('')
+  // let location;
+  return (
+    <div className={styles.geolocation}>
+      <label> Mестoположение: </label>
+
+      <label> </label>
+      <button
+        onClick={getLocation}
+        className={styles['geolocation__serch-button']}>
+        {location ? 'Обновить' : 'Найти'}
+      </button>
+    </div>
+  )
+}
